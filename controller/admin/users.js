3b4faa18e752151b45bb26289281097f7914d9e5
Sunday Omolewu <omolewu12@gmail.com>
2023-02-21 12:28:38 +0100

    SQL CRUD

--- controller/admin/users.js
+++ controller/admin/users.js
@@ -9,6 +9,44 @@ exports.postUser=(req, res)=>{
       res.redirect(302, '/admin');
     }).catch(err=>{
         console.log(err)
+    })  
+}
+
+exports.adminHome =(req, res)=>{
+    db.execute("SELECT * from registration")
+    .then(result=>{ 
+        res.render('admin/index', {users: result[0]});
+    }).catch(err=>{
+        console.log(err)
+    })
+ 
+}
+exports.updateuser=(req, res)=>{
+    let id = req.params.id
+    db.execute("selecT * FROm registration WHERE id=?",[id])
+    .then(data =>{
+      
+      res.render('admin/update-user', {user:data[0]} )
+    })
+    .catch(err=>{
+        console.log(err)
     })
    
+}
+
+exports.adminUpdate = (req, res)=>{
+const {id , name, phone, image} =  req.body
+db.execute("UPDATE registration SET name=?, phone=?, image=? WHERE id=?", [name, phone, image, id])
+.then(result =>{
+    res.redirect(302, '/admin');
+})
+}
+exports.deleteUser =(req, res)=>{
+    let {id}= req.body; 
+    db.execute("Delete  FROM registration WHERE id=?", [id])
+    .then(result =>{
+        res.redirect(302, '/admin');
+    }).catch(err =>{
+        console.log(err);
+    })
 }
--- routes/admin/user.js
+++ routes/admin/user.js
@@ -3,4 +3,8 @@ const adminUserController = require('../../controller/admin/users')
 
 router.get('/add-user', adminUserController.addUser)
 router.post('/add-user', adminUserController.postUser)
+router.get('/admin', adminUserController.adminHome);
+router.get('/update-user/:id', adminUserController.updateuser)
+router.post('/update-user', adminUserController.adminUpdate)
+router.post('/delete-user', adminUserController.deleteUser)
 module.exports=router;
